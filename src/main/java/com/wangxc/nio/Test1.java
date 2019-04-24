0dd3b7716e3099213d2d9dfe80c66baf51063017
wangxc <1013215849@qq.com>
2019-04-24 22:28:05 +0800

    update .ignore.

--- src/main/java/com/wangxc/nio/Test1.java
+++ src/main/java/com/wangxc/nio/Test1.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 public class Test1 {
     public static void main(String[] args) {
         ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[]{1, 2, 3});
+
         System.out.println("byteBuffer.getClass().getName() = " + byteBuffer.getClass().getName());
         System.out.println("byteBuffer.capacity() = " + byteBuffer.capacity());
         System.out.println("byteBuffer.limit() = " + byteBuffer.limit());
